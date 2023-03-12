ffe96112a7e94819858cb5a8ee15dee88778f64f
fabianp60 <fabianp60@gmail.com>
2023-03-11 21:21:02 -0500

    ajustes por tablet

--- site.js
+++ site.js
@@ -57,13 +57,7 @@ function createWordButton(word) {
     const template = `
         <div class="col-12 col-md-2 mb-2">
             <div class="word-btn card text-bg-primary" data-word="${word}">
-            <div class="img-mini me-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
-                <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
-                </svg>
-            </div>
-            <h5>${word}</h5>
+                <h5>${word}</h5>
             </div>
         </div>`;
     const divObj = document.createElement("div");
